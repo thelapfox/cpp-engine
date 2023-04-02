c58a4805490445c55334a558000ba88cacdb1875
thelapfox <wilmonman1996@gmail.com>
2023-04-01 20:29:57 -0500

    Event system in main working

---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by thelapfox on 3/31/2023.
+//
+
+#include "test.h"
+
+namespace Engine {
+    namespace Core {
+    } // Engine
+} // Core
---
+++ src/test.h
@@ -0,0 +1,18 @@
+//
+// Created by thelapfox on 3/31/2023.
+//
+
+#ifndef CPP_ENGINE_TEST_H
+#define CPP_ENGINE_TEST_H
+
+namespace Engine {
+    namespace Core {
+
+        class test {
+
+        };
+
+    } // Engine
+} // Core
+
+#endif //CPP_ENGINE_TEST_H
